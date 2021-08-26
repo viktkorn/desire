0c2bb2422d332bbaa7afc3b3b08b784bb7d41c7b
Viktoria Kornienko <89094100+viktkorn@users.noreply.github.com>
2021-08-26 22:44:52 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,30 @@
+$(function () {
+    $('.header__btn').on('click', function() {
+        $('.rightside-menu').removeClass('rightside-menu--close');
+    });
+    $('.rightside-menu__close').on('click', function() {
+        $('.rightside-menu').addClass('rightside-menu--close');
+    });
+    $('.top__slider').slick({
+        dots: true,
+        arrows: false,
+        fade: true,
+        autoplay: true
+    });
+    $('.contact-slider').slick({
+        slidesToShow: 10,
+        slidesToScroll: 10,
+        dots: true,
+        arrows: false
+    });
+    $('.article-slider__box').slick({
+        prevArrow:'<button type="button" class="article-slider__arrow article-slider__arrowLeft"><img src="images/arrow-slide-left.svg" alt="arrow left"></button>',
+        nextArrow:'<button type="button" class="article-slider__arrow article-slider__arrowRight"><img src="images/arrow-slide-right.svg" alt="arrow right"></button>'
+
+    });
+    var mixer = mixitup('.gallery__inner',{
+        load: {
+            filter: '.living'
+        }
+    });
+})
