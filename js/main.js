783c4d80c56b335f3c45995cac1ceda8d3698416
Viktoria Kornienko <89094100+viktkorn@users.noreply.github.com>
2021-09-17 22:27:36 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -5,6 +5,15 @@ $(function () {
     $('.rightside-menu__close').on('click', function() {
         $('.rightside-menu').addClass('rightside-menu--close');
     });
+
+    $('.header__btn-menu').on('click', function() {
+        $('.menu').toggleClass('menu--open');
+    });
+
+    if($(window).width() < 651){
+       $('.works-path__item--Measurements').appendTo($('.works-path__items-box'));
+    }
+
     $('.top__slider').slick({
         dots: true,
         arrows: false,
@@ -15,7 +24,30 @@ $(function () {
         slidesToShow: 10,
         slidesToScroll: 10,
         dots: true,
-        arrows: false
+        arrows: false,
+        responsive: [
+    {
+      breakpoint: 1700,
+      settings: {
+        slidesToShow: 8,
+        slidesToScroll: 8,
+      }
+    },
+    {
+      breakpoint: 1511,
+      settings: {
+        slidesToShow: 6,
+        slidesToScroll: 6,
+      }
+    },
+    {
+      breakpoint: 1201,
+      settings: {
+        slidesToShow: 4,
+        slidesToScroll: 4,
+      }
+    }
+  ]
     });
     $('.article-slider__box').slick({
         prevArrow:'<button type="button" class="article-slider__arrow article-slider__arrowLeft"><img src="images/arrow-slide-left.svg" alt="arrow left"></button>',
